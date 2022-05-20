15ef9f3c23ef37fce8308d6058c5b80786962ece
Taranpreet Singh <taran99.singh@gmail.com>
2022-05-19 20:23:05 -0400

    Tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,13 @@
+#Tuples practice
+
+student1 = ("Taran",22, "Software Developer")
+student2 = ("John",23, "Architect")
+
+print(student1[0])
+
+
+
+def printTuple(tup):
+    for i in tup:
+        print(i)
+printTuple(student1)
